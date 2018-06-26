a644b083457fff63384af5e6e415d7e9cfb59c88
che <che.pasha@gmail.com>
2018-06-27 02:06:11 +0300

    New deploy has been defined

--- deploy.sh
+++ deploy.sh
@@ -6,14 +6,14 @@ PROIT_TMP_DIR="$PROIT_DIR/tmp"
 
 SITE_BRANCH='master'
 SITE_DIR="$PROIT_DIR/site"
-SITE_URL="https://github.com/pro-it/site/archive/$SITE_BRANCH.tar.gz"
+SITE_URL="https://codeload.github.com/pro-it/site/tar.gz/$SITE_BRANCH"
 
 DEPLOY_BRANCH='master'
-DEPLOY_URL="https://github.com/pro-it/deploy/archive/$DEPLOY_BRANCH.tar.gz"
+DEPLOY_URL="https://codeload.github.com/pro-it/deploy/tar.gz/$DEPLOY_BRANCH"
 
 FEE_DIR="$PROIT_DIR/fee"
 FEE_BRANCH='master'
-FEE_URL="https://github.com/pro-it/fee/archive/$FEE_BRANCH.tar.gz"
+FEE_URL="https://codeload.github.com/pro-it/fee/tar.gz/$FEE_BRANCH"
 
 #NGINX_APT_FILE='/etc/apt/sources.list.d/nginx.list'
 NGINX_ETC_DIR='/etc/nginx'
@@ -21,10 +21,13 @@ NGINX_DIR="$PROIT_DIR/nginx"
 
 LETSENCRYPT_DIR="$PROIT_DIR/letsencrypt"
 LETSENCRYPT_BRANCH='master'
-LETSENCRYPT_URL="https://github.com/letsencrypt/letsencrypt/archive/$LETSENCRYPT_BRANCH.tar.gz"
+LETSENCRYPT_URL="https://codeload.github.com/certbot/certbot/tar.gz/$LETSENCRYPT_BRANCH"
 
-PROIT_DHPARAM_KEY_FILE="$LETSENCRYPT_DIR/dhparam.pem"
-PROIT_SSL_BIT_KEY_SIZE=4096
+SSL_DIR="$PROIT_DIR/ssl"
+SSL_TICKET_KEY_FILE="$SSL_DIR/ticket.key"
+SSL_TICKET_KEY_BIT_SIZE=80
+SSL_DHPARAM_FILE="$SSL_DIR/dhparam.pem"
+SSL_DHPARAM_BIT_SIZE=4096
 
 
 structure_define()
@@ -33,6 +36,7 @@ structure_define()
 
     mkdir -p "$PROIT_TMP_DIR"
     mkdir -p "$SITE_DIR"
+    mkdir -p "$SSL_DIR"
     mkdir -p "$NGINX_DIR"
     mkdir -p "$LETSENCRYPT_DIR"
 }
@@ -42,6 +46,7 @@ structure_remove()
     rm -rf "$PROIT_TMP_DIR"
     rm -rf "$FEE_DIR"
     rm -rf "$SITE_DIR"
+    rm -rf "$SSL_DIR"
     rm -rf "$NGINX_DIR"
     rm -rf "$LETSENCRYPT_DIR"
 }
@@ -62,7 +67,20 @@ deploy_download()
 # DHParam
 openssl_dhparam_define()
 {
-    openssl dhparam -out "$PROIT_DHPARAM_KEY_FILE" "$PROIT_SSL_BIT_KEY_SIZE"
+    openssl dhparam -out "$SSL_DHPARAM_FILE" "$SSL_DHPARAM_BIT_SIZE"
+}
+
+# SSL Ticket key generate
+openssl_ticket_key_define()
+{
+    openssl rand "$SSL_TICKET_KEY_BIT_SIZE" > "$SSL_TICKET_KEY_FILE"
+}
+
+# SSL
+ssl_define()
+{
+    openssl_ticket_key_define && \
+    openssl_dhparam_define
 }
 
 nginx_define()
@@ -78,8 +96,6 @@ nginx_define()
         echo "$f"
         sudo ln -sf "$NGINX_DIR/$f" "$NGINX_ETC_DIR/conf.d"
     done
-
-    openssl_dhparam_define
 }
 
 letsencrypt_define()
@@ -101,11 +117,12 @@ deploy_define()
 {
     local deploy_dir="$PROIT_TMP_DIR/deploy-$DEPLOY_BRANCH/"
 
-    mv "$deploy_dir/letsencrypt" "$PROIT_DIR/"
-    mv "$deploy_dir/nginx" "$PROIT_DIR/"
-    rm -rf "$deploy_dir/"
+    mv "$deploy_dir/letsencrypt" "$PROIT_DIR/" && \
+    mv "$deploy_dir/nginx" "$PROIT_DIR/" && \
+    rm -rf "$deploy_dir/" && \
 
-    nginx_define
+    ssl_define && \
+    nginx_define && \
     letsencrypt_define
 }
 
@@ -156,17 +173,17 @@ site_define()
 
 deploy()
 {
-    structure_remove
-    structure_define
+    structure_remove && \
+    structure_define && \
 
-    fee_download
-    fee_define
+    fee_download && \
+    fee_define && \
 
-    deploy_download
-    deploy_define
+    deploy_download && \
+    deploy_define && \
 
-    site_download
-    site_define
+    site_download && \
+    site_define && \
 
     rm -rf "$PROIT_TMP_DIR"
 }
