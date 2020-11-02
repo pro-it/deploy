21ce1a7f8bbf78cd2b6addddc439c03a8badd10d
che <che.pasha@gmail.com>
2020-11-02 08:46:09 +0300

    Demo cofig has been defined

--- letsencrypt/update/prgit.sh
+++ letsencrypt/update/prgit.sh
@@ -5,18 +5,6 @@
     --email info@prgit.by \
     --renew-by-default \
     --rsa-key-size 4096 \
-    -d prgit.by \
-    -d charter.prgit.by \
-    -d chat.prgit.by \
-    -d docs.prgit.by \
-    -d fee.prgit.by \
-    -d info.prgit.by \
-    -d member.prgit.by \
-    -d logo.prgit.by \
-    -d ptuo.prgit.by \
-    -d tug.prgit.by \
-    -d size.fee.prgit.by \
-    -d v1.charter.prgit.by \
-    -d www.prgit.by \
+    -d demo.prgit.by \
     --pre-hook 'systemctl stop nginx' \
     --post-hook 'systemctl start nginx'
--- letsencrypt/update/proit.sh
+++ letsencrypt/update/proit.sh
@@ -5,18 +5,6 @@
     --email info@xn--h1alcem.xn--90ais \
     --renew-by-default \
     --rsa-key-size 4096 \
-    -d xn--h1alcem.xn--90ais \
-    -d www.xn--h1alcem.xn--90ais \
-    -d xn--1-ctb.xn--80ae7bed.xn--h1alcem.xn--90ais \
-    -d xn--b1aksep.xn--h1alcem.xn--90ais \
-    -d xn--h1akdx.xn--h1alcem.xn--90ais \
-    -d xn--d1aifp.xn--h1alcem.xn--90ais \
-    -d xn--c1apjb.xn--h1alcem.xn--90ais \
-    -d xn--n1aba.xn--h1alcem.xn--90ais \
-    -d xn--80akfure.xn--b1aksep.xn--h1alcem.xn--90ais \
-    -d xn--c1ax.xn--h1alcem.xn--90ais \
-    -d xn--e1alh1b.xn--h1alcem.xn--90ais \
-    -d xn--80ae7bed.xn--h1alcem.xn--90ais \
-    -d xn--80a0bn.xn--h1alcem.xn--90ais \
+    -d xn--d1acvi.xn--h1alcem.xn--90ais \
     --pre-hook 'systemctl stop nginx' \
     --post-hook 'systemctl start nginx'
